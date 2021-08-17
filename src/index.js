2c16471e2e7b68d700520e859f2057ac2d8e4c3b
阳光彩虹小白🐷 <oncwnuH-vJ7B8qyq3pz9EAXsMN6c@git.weixin.qq.com>
2021-08-17 14:47:55 +0800

    recursively create assets dir, add assetPath

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'fs';
-import { basename, dirname } from 'path';
+import { basename, dirname, resolve } from 'path';
 import { createFilter } from 'rollup-pluginutils';
 
 const defaultExtensions = ['.jpg', '.jpeg', '.png', '.gif', '.svg'];
@@ -7,17 +7,21 @@ const defaultExtensions = ['.jpg', '.jpeg', '.png', '.gif', '.svg'];
 export default function image(options = {}) {
 	const extensions = options.extensions || defaultExtensions;
 	const includes = extensions.map(e => `**/*${e}`);
+	const assetPath = options.assetPath || '';
 	const filter = createFilter(options.include || includes, options.exclude);
 	let images = [];
 
 	function generateBundle(outputOptions, rendered) {
-		const dir =
-			outputOptions.dir || dirname(outputOptions.dest || outputOptions.file);
+
+		const dir = outputOptions.dir
+			? resolve(outputOptions.dir, assetPath)
+			: dirname(outputOptions.dest || outputOptions.file);
+
 		if (!existsSync(dir)) {
-			mkdirSync(dir);
+			mkdirSync(dir, { recursive: true });
 		}
 		images.forEach(id => {
-			writeFileSync(`${dir}/${basename(id)}`, readFileSync(id));
+			writeFileSync(resolve(dir, basename(id)), readFileSync(id));
 		});
 	}
 
@@ -31,7 +35,9 @@ export default function image(options = {}) {
 			if (images.indexOf(id) < 0) {
 				images.push(id);
 			}
-			return `const img = require('./${basename(id)}'); export default img;`;
+			return `const img = require('./${assetPath}${basename(
+				id
+			)}'); export default img;`;
 		},
 		generateBundle,
 		ongenerate: generateBundle
